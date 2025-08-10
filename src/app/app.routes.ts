95465001c36097be58312f2a30232c36b4e721f8
Shubham Jagdale <124193928+shubhamj974@users.noreply.github.com>
2025-08-10 18:50:59 +0530

    landing inprogress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'seller',
+    loadChildren: () =>
+      import('./features/seller/seller.module').then((m) => m.SellerModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/customer//customer.routes').then(
+        (m) => m.CustomerRoutes
+      ),
+  },
+  {
+    path: 'delivery',
+    loadChildren: () =>
+      import('./features/delivery/delivery.module').then(
+        (m) => m.DeliveryModule
+      ),
+  },
+  {
+    path: 'support',
+    loadChildren: () =>
+      import('./features/support/suppot.module').then((m) => m.SuppotModule),
+  },
+  {
+    path: '',
+    redirectTo: '', // or a public landing page
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '', // fallback route
+  },
+];
