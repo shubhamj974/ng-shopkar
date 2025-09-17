264bc51907a95af931e115dd167f5a7a5317a6d4
shubham <shubham@stellaripl.com>
2025-09-17 19:54:48 +0530

    changes

--- src/app/features/customer/product-list/product-list.component.ts
+++ src/app/features/customer/product-list/product-list.component.ts
@@ -33,6 +33,27 @@ export class ProductListComponent {
       bankOffer: 'Bank Offer',
       exchangeOffer: 'Upto ₹9,100 Off on Exchange'
     },
+     {
+      id: 'MOBH8VGV88UADK2Z',
+      name: 'realme P3x 5G (Midnight Blue, 128 GB)',
+      imageUrl: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/j/n/1/-original-imah9gtmya9qhqse.jpeg?q=70',
+      rating: 4.4,
+      reviews: 1163,
+      price: 12999,
+      originalPrice: 16999,
+      discount: '23% off',
+      specs: [
+        '6 GB RAM | 128 GB ROM',
+        '17.07 cm (6.72 inch) Full HD Display',
+        '50MP Rear Camera | 8MP Front Camera',
+        '6000 mAh Battery',
+        'Dimensity 6400 Processor',
+        '1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for Inbox Accessories'
+      ],
+      badge: 'Bestseller',
+      bankOffer: 'Bank Offer',
+      exchangeOffer: 'Upto ₹9,100 Off on Exchange'
+    },
   ];
 }
 
