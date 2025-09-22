27a1271a2f21a3233a7ceb2b61f3603d7387f5de
shubham <shubham@stellaripl.com>
2025-09-22 22:13:36 +0530

    changes

--- src/app/features/customer/category-menu/category-menu.component.ts
+++ src/app/features/customer/category-menu/category-menu.component.ts
@@ -5,11 +5,22 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-category-menu',
   standalone: true,
-  imports: [RouterLink,JsonPipe],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './category-menu.component.html',
   styleUrl: './category-menu.component.scss',
 })
 export class CategoryMenuComponent {
-  @Input() cate_menu : Array<any> = []
+  @Input() cate_menu: Array<any> = []
   activeSubcategory: any = null;
+
+
+  syncDropdownHeights() {
+    setTimeout(() => {
+      const dropdowns = document.querySelectorAll<HTMLElement>('.dropdown, .dropdown-right');
+      dropdowns.forEach(d => d.style.height = 'auto');
+      const maxHeight = Math.max(...Array.from(dropdowns).map(d => d.offsetHeight));
+      dropdowns.forEach(d => d.style.height = maxHeight + 'px');
+    }, 0);
+  }
+
 }
