a9a4de7256096a1494b0af1ae2cc8c945a8a3e99
shubham <shubham@stellaripl.com>
2025-10-07 19:35:13 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   //   apiUrl: 'https://dev.shopkar.com',
   //   apiUrl: 'https://shopkar.com',
-  apiUrl: 'http://localhost:3009',
-  // apiUrl : 'https://shopkar-api-production.up.railway.app'
+  // apiUrl: 'http://localhost:3009',
+  apiUrl : 'https://shopkar-api-production.up.railway.app'
 };
